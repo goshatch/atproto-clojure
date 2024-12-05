0dfb55c704a2d5818b5fe572c446e19afc548649
Chase Lambert <chaselambert@gmail.com>
2024-12-05 13:38:31 -0800

    finally fixed it (I think)! Switched to json instead of cbor using a different firehose source

--- src/net/gosha/atproto/firehose.clj
+++ src/net/gosha/atproto/firehose.clj
@@ -2,95 +2,82 @@
   (:require
    [clojure.core.async    :as async]
    [clojure.tools.logging :as log]
-   [clj-cbor.core         :as cbor])
+   [clojure.data.json     :as json])
   (:import
-   [java.io ByteArrayInputStream]
-   [java.nio ByteBuffer]
    [java.net URI]
-   [org.java_websocket.client WebSocketClient]
-   [org.java_websocket.handshake ServerHandshake]))
+   [org.java_websocket.client WebSocketClient]))
 
-(defn read-varint
-  "Read a variable-length integer from the input stream"
-  [^ByteArrayInputStream in]
-  (loop [shift 0
-         result 0]
-    (let [b (.read in)]
-      (if (neg? b)
-        (throw (ex-info "EOF while reading varint" {})))
-      (let [val    (bit-and b 0x7F)
-            result (bit-or result (bit-shift-left val shift))]
-        (if (zero? (bit-and b 0x80))
-          result
-          (recur (+ shift 7) result))))))
-
-(defn decode-frame
-  "Decode a single frame from binary data"
-  [^ByteBuffer buffer]
-  (try 
-    (let [bytes (byte-array (.remaining buffer))]
-      (.get buffer bytes)
-      (with-open [in (ByteArrayInputStream. bytes)]
-        (let [frame-len   (read-varint in)
-              frame-bytes (byte-array frame-len)]
-          (.read in frame-bytes 0 frame-len)
-          (let [decoded (cbor/decode frame-bytes)]
-            {:header {:t  (get decoded "t")}
-                     :op (get decoded "op")
-             :body   decoded}))))
+(defn parse-firehose-message
+  "Parse a firehose message into a structured format"
+  [message]
+  (try
+    (let [data (json/read-str message :key-fn keyword)]
+      (case (:kind data)
+        "commit" 
+        {:type       :commit
+         :did        (:did data)
+         :timestamp  (:time_us data)
+         :rev        (get-in data [:commit :rev])
+         :operation  (get-in data [:commit :operation])
+         :collection (get-in data [:commit :collection])
+         :record     (get-in data [:commit :record])
+         :raw        data}
+        
+        {:type :unknown
+         :data data}))
     (catch Exception e
-      (log/error "Frame decode error:" (.getMessage e))
+      (log/error "Parse error:" (.getMessage e))
       nil)))
 
 (defn create-websocket-client
-  [{:keys [uri output-ch]}]
-  (proxy [WebSocketClient] [(URI. uri)]
-    (onOpen [^ServerHandshake handshake]
-      (log/info "Connected to firehose"))
-    
-    (onClose [code reason remote]
-      (log/info "Disconnected from firehose:" reason))
-    
-    (onMessage [^ByteBuffer buffer]
-      (when-let [frame (decode-frame buffer)]
-        (async/>!! output-ch frame)))
-    
-    (onError [^Exception ex]
-      (log/error "WebSocket error:" (.getMessage ex)))))
+  [uri output-ch]
+  (doto 
+    (proxy [WebSocketClient] [(URI. (str uri "?wantedCollections=app.bsky.feed.post"))]
+      (onOpen [_]
+        (log/info "Connected to jetstream"))
+      
+      (onClose [code reason remote]
+        (log/info "Disconnected from jetstream:" reason))
+      
+      (onMessage [message]
+        (when-let [parsed (parse-firehose-message message)]
+          (async/>!! output-ch parsed)))
+      
+      (onError [^Exception ex]
+        (log/error "WebSocket error:" (.getMessage ex))))
+    (.setConnectionLostTimeout 60)))
 
 (defn connect-firehose
   "Connect to ATProto firehose and return a channel of events"
   [& {:keys [buffer-size service]
       :or   {buffer-size 1024
-             service     "wss://bsky.network"}}]
+             service     "wss://jetstream2.us-east.bsky.network"}}]
   (let [output-ch (async/chan buffer-size)
-        uri       (str service "/xrpc/com.atproto.sync.subscribeRepos")
-        client    (create-websocket-client
-                   {:uri       uri
-                    :output-ch output-ch})]
+        uri       (str service "/subscribe")
+        client    (create-websocket-client uri output-ch)]
     
     (.connect client)
     
     {:client client
      :events output-ch}))
 
-(defn disconnect 
+(defn disconnect
   [{:keys [client events]}]
   (when client 
-    (.close client)
-    (log/info "Disconnected from firehose"))
+    (.close client))
   (when events 
     (async/close! events)))
 
 (comment
-  ;; Example usage with timeout to avoid hanging
+  ;; Example usage
   (def conn (connect-firehose))
   
-  ;; Get one message with timeout
   (let [event (async/alt!!
                 (:events conn) ([v] v)
-                (async/timeout 5000) nil)]
-    (when event
-      (println "\nReceived event:" (pr-str event))))
+                (async/timeout 5000) :timeout)]
+    (println "\nReceived event:" 
+             {:type       (:type event)
+              :collection (:collection event)
+              :text      (get-in event [:record :text])}))
   
   (disconnect conn))
