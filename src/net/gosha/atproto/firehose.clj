7ffc2d06563ba9c5ec2c76f225cd07b35bd333d9
Chase Lambert <chaselambert@gmail.com>
2024-12-05 12:49:48 -0800

    latest attempts to properly decode incoming data, still failing...

--- src/net/gosha/atproto/firehose.clj
+++ src/net/gosha/atproto/firehose.clj
@@ -4,58 +4,46 @@
    [clojure.tools.logging :as log]
    [clj-cbor.core         :as cbor])
   (:import
-   [java.io  ByteArrayInputStream InputStream]
+   [java.io ByteArrayInputStream]
    [java.nio ByteBuffer]
    [java.net URI]
    [org.java_websocket.client WebSocketClient]
    [org.java_websocket.handshake ServerHandshake]))
 
-(defn buffer->bytes
-  "Convert a ByteBuffer to a byte array"
-  [^ByteBuffer buffer]
-  (let [bytes (byte-array (.remaining buffer))]
-    (.get buffer bytes)
-    bytes))
-
-(defn decode-varint
-  "Decode a variable-length integer from an input stream"
-  [^InputStream in]
-  (loop [shift  0
+(defn read-varint
+  "Read a variable-length integer from the input stream"
+  [^ByteArrayInputStream in]
+  (loop [shift 0
          result 0]
-    (let [byte (.read in)]
-      (if (neg? byte)
+    (let [b (.read in)]
+      (if (neg? b)
         (throw (ex-info "EOF while reading varint" {})))
-      (let [result (bit-or result (bit-shift-left (bit-and byte 0x7f) shift))]
-        (if (zero? (bit-and byte 0x80))
+      (let [val    (bit-and b 0x7F)
+            result (bit-or result (bit-shift-left val shift))]
+        (if (zero? (bit-and b 0x80))
           result
           (recur (+ shift 7) result))))))
 
 (defn decode-frame
-  "Decode a single CBOR frame from binary data"
-  [data codec]
-  (try
-    (let [bytes (if (instance? ByteBuffer data)
-                  (buffer->bytes data)
-                  data)]
+  "Decode a single frame from binary data"
+  [^ByteBuffer buffer]
+  (try 
+    (let [bytes (byte-array (.remaining buffer))]
+      (.get buffer bytes)
       (with-open [in (ByteArrayInputStream. bytes)]
-        (let [length    (decode-varint in)
-              remaining (.available in)]
-          (log/debug "Frame length:" length "remaining:" remaining)
-          (when (>= remaining length)
-            (let [buffer (byte-array length)
-                  bytes-read (.read in buffer 0 length)]
-              (when (= bytes-read length)
-                (with-open [frame-stream (ByteArrayInputStream. buffer)]
-                  (let [decoded (cbor/decode codec frame-stream)]
-                    (log/debug "Decoded frame type:" (type decoded))
-                    decoded))))))))
+        (let [frame-len   (read-varint in)
+              frame-bytes (byte-array frame-len)]
+          (.read in frame-bytes 0 frame-len)
+          (let [decoded (cbor/decode frame-bytes)]
+            {:header {:t  (get decoded "t")}
+                     :op (get decoded "op")
+             :body   decoded}))))
     (catch Exception e
-      (log/error e "Error decoding frame")
+      (log/error "Frame decode error:" (.getMessage e))
       nil)))
 
 (defn create-websocket-client
-  "Create a WebSocket client for the ATProto firehose"
-  [{:keys [uri output-ch codec]}]
+  [{:keys [uri output-ch]}]
   (proxy [WebSocketClient] [(URI. uri)]
     (onOpen [^ServerHandshake handshake]
       (log/info "Connected to firehose"))
@@ -63,56 +51,46 @@
     (onClose [code reason remote]
       (log/info "Disconnected from firehose:" reason))
     
-    (onMessage [data]
-      (when-let [decoded (decode-frame data codec)]
-        (log/debug "Received message of type:" (type decoded))
-        (async/>!! output-ch decoded)))
+    (onMessage [^ByteBuffer buffer]
+      (when-let [frame (decode-frame buffer)]
+        (async/>!! output-ch frame)))
     
     (onError [^Exception ex]
-      (log/error ex "WebSocket error"))))
+      (log/error "WebSocket error:" (.getMessage ex)))))
 
 (defn connect-firehose
-  "Connect to ATProto firehose and return a channel of decoded events.
-   
-   Options:
-   - :buffer-size - Size of the async buffer (default 1024)
-   - :service     - Service URL (default wss://bsky.network)
-   - :codec       - CBOR codec (default cbor/default-codec)"
-  [& {:keys [buffer-size service codec]
+  "Connect to ATProto firehose and return a channel of events"
+  [& {:keys [buffer-size service]
       :or   {buffer-size 1024
-             service     "wss://bsky.network"
-             codec       cbor/default-codec}}]
-  
+             service     "wss://bsky.network"}}]
   (let [output-ch (async/chan buffer-size)
         uri       (str service "/xrpc/com.atproto.sync.subscribeRepos")
         client    (create-websocket-client
                    {:uri       uri
-                    :output-ch output-ch
-                    :codec     codec})]
+                    :output-ch output-ch})]
     
-    (log/info "Connecting to firehose at" uri)
     (.connect client)
     
     {:client client
      :events output-ch}))
 
-(defn disconnect
-  "Cleanly disconnect firehose client"
+(defn disconnect 
   [{:keys [client events]}]
-  (when client
-    (.close client))
-  (when events
+  (when client 
+    (.close client)
+    (log/info "Disconnected from firehose"))
+  (when events 
     (async/close! events)))
 
 (comment
-  ;; Test connection
+  ;; Example usage with timeout to avoid hanging
   (def conn (connect-firehose))
   
-  ;; Print raw event type and structure
-  (let [event (async/<!! (:events conn))]
-    (println "Event type:" (type event))
-    (println "Event str:" (str event))
-    (println "Event pr-str:" (pr-str event)))
+  ;; Get one message with timeout
+  (let [event (async/alt!!
+                (:events conn) ([v] v)
+                (async/timeout 5000) nil)]
+    (when event
+      (println "\nReceived event:" (pr-str event))))
   
-  ;; Cleanup
   (disconnect conn))
